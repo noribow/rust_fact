d25dd13fcb8af1928fa6f823edfe4664ce50ed58
Takanori SHIRAI <shirai@mail.yunarin.com>
2021-08-09 11:04:55 +0900

    FIX: BigNum struct ADD: BigNum::set

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 struct BigNum {
     len : u32,
-    flag : u32,
+    sign : i32,
     m : Vec<u32>,
 }
 
@@ -9,7 +9,20 @@ impl BigNum {
         let mut p : Vec<u32> = Vec::with_capacity(256);
         p.resize(256, 0);
         p[0] = 0; 
-        BigNum{ len:1, flag:0, m:p}
+        BigNum{ len:1, sign:0, m:p}
+    }
+    pub fn set( &mut self, _a:i32) {
+        self.len = 1;
+        if _a > 0 {
+            self.sign = 1;
+            self.m[0] = _a.clone() as u32;
+        } else if _a < 0 {
+            self.sign = -1;
+            self.m[0] = _a.abs() as u32;
+        }else {
+            self.sign = 0;
+            self.m[0] = 0
+        }
     }
 }
 
@@ -24,7 +37,7 @@ mod tests {
     fn new_test() {
         let a = BigNum::new();
         assert_eq!(a.len, 1);
-        assert_eq!(a.flag, 0);
+        assert_eq!(a.sign, 0);
         assert_eq!(a.m.capacity(), 256);
         assert_eq!(a.m.len(), 256);
         assert_eq!(a.m[0], 0)
